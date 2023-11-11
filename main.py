4d7b2ac2156a166c393f085703ddb9cc867a4553
Andrei Fedotov <andrei.fedotov@idp.zyfra.com>
2023-11-11 03:15:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,37 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+wagons = []
+
+
+def get_wagons_data():
+    if wagons:
+        return wagons
+
+    wagons_data_path = "data/disl_hackaton.xlsx"
+    wb = openpyxl.load_workbook(filename=wagons_data_path, read_only=True, keep_vba=False)
+    sheets = wb.sheetnames
+    i = 1
+    for s_name in sheets:
+        sheet = wb[s_name]
+        for i in range(2, sheet.max_row):
+            train_info_value = sheet.cell(row=i, column=5).value
+            train_info = ""
+            if train_info_value != "":
+                train_info = train_info_value.split("-")
+            wagon = {
+                "wagonId": sheet.cell(row=i, column=1).value,
+                "currentStationId": sheet.cell(row=i, column=3).value,
+                "arrivalTime": sheet.cell(row=i, column=2).value,
+                "wagonStartStation": train_info[0],
+                "wagonDestination": sheet.cell(row=i, column=4).value,
+                "trainDestination": train_info[2],
+                "trainNumber": train_info[1]
+            }
+            wagons.append(wagon)
+
+    return wagons
+
 
 def get_stations_data():
     stations = []
@@ -20,6 +51,9 @@ def get_stations_data():
     return stations
 
 
+# get_wagons_data()
+
+
 @app.route("/stations")
 def get_stations():
     stations = get_stations_data()
@@ -27,5 +61,12 @@ def get_stations():
     return jsonify(stations), 200
 
 
+@app.route("/wagons")
+def get_wagons():
+    wagons_data = get_wagons_data()
+
+    return jsonify(wagons_data), 200
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
